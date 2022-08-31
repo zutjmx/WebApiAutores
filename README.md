7c54b7942ca76bed38870604a936bdd9928bebca
Jesus Zuñiga <jesuszt@hotmail.com>
2022-08-31 16:28:41 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebApiAutores
+## Proyecto Web Api en .Net Core 6 para el curso Construyendo Web APIs RESTful con ASP.NET Core 6
---
+++ WebApiAutores/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace WebApiAutores
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder application, IWebHostEnvironment hostEnvironment)
+        {
+            // Configure the HTTP request pipeline.
+            if (hostEnvironment.IsDevelopment())
+            {
+                application.UseSwagger();
+                application.UseSwaggerUI();
+            }
+
+            application.UseHttpsRedirection();
+            application.UseRouting();
+            application.UseAuthorization();
+
+            application.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
