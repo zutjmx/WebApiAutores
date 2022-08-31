a87c3713ce05416d55bd15b57b0b557b463f642c
Jesus Zuñiga <jesuszt@hotmail.com>
2022-08-31 17:09:32 -0500

    Se agrega controlador para autores, modelo, clase Util y paquete Faker

---
+++ WebApiAutores/Common/Util.cs
@@ -0,0 +1,26 @@
+﻿using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Common
+{
+    public class Util
+    {
+        public List<Autor> GetAutors(int Cuantos) {
+            List<Autor> autors = new List<Autor>();
+            for (int j = 0; j <= Cuantos; j++)
+            {
+                Autor autor = new Autor();
+                autor.Id = Faker.RandomNumber.Next(100);
+                autor.Nombre = Faker.Name.FullName();
+                autors.Add(autor);
+            }
+            return autors; 
+        }
+
+        public Autor GetAutor(int id) {
+            Autor autor = new Autor();
+            autor.Id = id;
+            autor.Nombre = Faker.Name.FullName();
+            return autor;
+        }
+    }
+}
---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Common;
+using WebApiAutores.Entidades;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiAutores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        // GET: api/<AutoresController>
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            Util util = new Util();
+            List<Autor> listaAutores = util.GetAutors(20);
+            return listaAutores;
+        }
+
+        // GET api/<AutoresController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Autor> Get(int id)
+        {
+            Util util = new Util();
+            Autor autor = util.GetAutor(id);
+            return autor;
+        }
+
+        // POST api/<AutoresController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AutoresController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AutoresController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
