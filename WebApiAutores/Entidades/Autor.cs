bf3d48ddb24d370e6b8ad3379710d12ccc68c0f8
Jesus Zuñiga <jesuszt@hotmail.com>
2022-11-11 10:09:38 -0600

    Repaso de como configurar EF Core

--- WebApiAutores/Entidades/Autor.cs
+++ WebApiAutores/Entidades/Autor.cs
@@ -9,7 +9,7 @@ namespace WebApiAutores.Entidades
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        [StringLength(maximumLength:60,ErrorMessage ="El campo {0} no puede tener más de {1} caracteres")]
+        [StringLength(maximumLength:90,ErrorMessage ="El campo {0} no puede tener más de {1} caracteres")]
         [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
 
--- WebApiAutores/Entidades/Libro.cs
+++ WebApiAutores/Entidades/Libro.cs
@@ -1,4 +1,5 @@
-﻿using WebApiAutores.Validaciones;
+﻿using System.ComponentModel.DataAnnotations;
+using WebApiAutores.Validaciones;
 
 namespace WebApiAutores.Entidades
 {
@@ -6,6 +7,7 @@ namespace WebApiAutores.Entidades
     {
         public int Id { get; set; }
 
+        [StringLength(maximumLength: 250, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
         [PrimeraLetraMayuscula]
         public string Titulo { get; set; }
     }
