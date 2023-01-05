f5ddf02190037919ac849ac8180c512c52b63800
Jesus Zuñiga <jesuszt@hotmail.com>
2023-01-05 16:37:38 -0600

    Se agrega proyecto de pruebas automatizadas

---
+++ WebApiAutores.Tests/PruebasUnitarias/RootControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiAutores.Controllers.V1;
+using WebApiAutores.Tests.Mocks;
+
+namespace WebApiAutores.Tests.PruebasUnitarias
+{
+    [TestClass]
+    public class RootControllerTests
+    {
+        [TestMethod]
+        public async Task SiUsuarioEsAdmin_Obtenemos4Links()
+        {
+            //Preparación
+            var authorizationService = new AuthorizationServiceMock();
+            authorizationService.Resultado = AuthorizationResult.Success();
+            var rootController = new RootController(authorizationService);
+            rootController.Url = new UrlHelperMock();
+
+            //Ejecución
+            var resultado = await rootController.Get();
+
+            //Verificación
+            Assert.AreEqual(4,resultado.Value.Count());
+
+        }
+
+        [TestMethod]
+        public async Task SiUsuarioNoEsAdmin_Obtenemos2Links()
+        {
+            //Preparación
+            var authorizationService = new AuthorizationServiceMock();
+            authorizationService.Resultado = AuthorizationResult.Failed();
+            var rootController = new RootController(authorizationService);
+            rootController.Url = new UrlHelperMock();
+
+            //Ejecución
+            var resultado = await rootController.Get();
+
+            //Verificación
+            Assert.AreEqual(2, resultado.Value.Count());
+
+        }
+
+    }
+}
