607e6b730237f4f1eae10ab0cde985876363dd36
Jesus Zuñiga <jesuszt@hotmail.com>
2023-01-05 17:10:07 -0600

    Fin de la sección 9.-Prueba Automáticas

--- WebApiAutores.Tests/PruebasUnitarias/RootControllerTests.cs
+++ WebApiAutores.Tests/PruebasUnitarias/RootControllerTests.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using WebApiAutores.Controllers.V1;
@@ -38,6 +41,40 @@ namespace WebApiAutores.Tests.PruebasUnitarias
             authorizationService.Resultado = AuthorizationResult.Failed();
             var rootController = new RootController(authorizationService);
             rootController.Url = new UrlHelperMock();
+
+            //Ejecución
+            var resultado = await rootController.Get();
+
+            //Verificación
+            Assert.AreEqual(2, resultado.Value.Count());
+
+        }
+
+        [TestMethod]
+        public async Task SiUsuarioNoEsAdmin_Obtenemos2Links_UsandoMoq()
+        {
+            //Preparación
+            var mockAuthorizationService = new Mock<IAuthorizationService>();
+            mockAuthorizationService.Setup(x => x.AuthorizeAsync(
+                It.IsAny<ClaimsPrincipal>(),
+                It.IsAny<object>(),
+                It.IsAny<IEnumerable<IAuthorizationRequirement>>()
+                )).Returns(Task.FromResult(AuthorizationResult.Failed()));
+
+            mockAuthorizationService.Setup(x => x.AuthorizeAsync(
+                It.IsAny<ClaimsPrincipal>(),
+                It.IsAny<object>(),
+                It.IsAny<string>()
+                )).Returns(Task.FromResult(AuthorizationResult.Failed()));
+
+            var mockUrlHelper = new Mock<IUrlHelper>();
+            mockUrlHelper.Setup(x => x.Link(
+                It.IsAny<string>(),
+                It.IsAny<object>()
+                )).Returns(string.Empty);
+
+            var rootController = new RootController(mockAuthorizationService.Object);
+            rootController.Url = mockUrlHelper.Object;
 
             //Ejecución
             var resultado = await rootController.Get();
